cb724bc78df6f1fc0cea24d5ea172d4f2756b55a
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-03-15 22:08:25 +0300

    The void State

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,6 +41,25 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         })),
         animate(500)
       ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          opacity: 0,
+          transform: 'translateX(100px)'
+        }))
+      ])
     ])
   ]
 })
